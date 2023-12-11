320cc72892ecd62f8ea6355a3bacc86bfb3d2ce5
JonathanStillman <68572893+JonathanStillman@users.noreply.github.com>
2023-12-11 10:01:36 -0600

    Update networking.tf

--- kubernetes/networking.tf
+++ kubernetes/networking.tf
@@ -3,20 +3,16 @@
 # Define networking components for the Kubernetes cluster
 
 resource "aws_vpc" "k8s_vpc" {
-  cidr_block = "10.0.0.0/16"  # Replace with your desired CIDR block
+  cidr_block = "10.0.0.0/16"
   enable_dns_support = true
   enable_dns_hostnames = true
-
-  # Add other VPC configurations if needed
 }
 
 resource "aws_subnet" "k8s_subnet" {
   vpc_id                  = aws_vpc.k8s_vpc.id
-  cidr_block             = "10.0.0.0/24"  # Replace with your desired CIDR block for the subnet
-  availability_zone       = "us-east-1a"  # Replace with your desired availability zone
+  cidr_block             = "10.0.0.0/24"
+  availability_zone       = "us-east-1a" 
   map_public_ip_on_launch = true
 
   # Add other subnet configurations if needed
 }
-
-# Add other networking resources (e.g., route tables, internet gateway, etc.)
