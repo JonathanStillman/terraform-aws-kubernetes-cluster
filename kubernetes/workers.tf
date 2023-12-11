52e41b59944101428bde5c08edafe80fa9107cc0
JonathanStillman <68572893+JonathanStillman@users.noreply.github.com>
2023-12-11 10:02:38 -0600

    Update workers.tf

--- kubernetes/workers.tf
+++ kubernetes/workers.tf
@@ -2,15 +2,13 @@
 
 resource "aws_instance" "k8s_workers" {
   count         = 3
-  ami           = "ami-0c55b159cbfafe1f0"  # Replace with your desired AMI
-  instance_type = "t2.micro"  # Replace with your desired instance type
-  key_name      = var.key_name.tnr-k8s-key  # Add your key name variable or replace it with your key name directly
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+  key_name      = var.key_name.tnr-k8s-key
 
   tags = {
     Name = "k8s-worker-${count.index + 1}"
   }
-
-  # Add other configuration for worker nodes if needed
 }
 
 resource "aws_security_group" "k8s_workers_sg" {
@@ -22,8 +20,4 @@ resource "aws_security_group" "k8s_workers_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-  # Add other security group rules if needed
 }
-
-# Add other resources for worker nodes (e.g., user data script, etc.)
